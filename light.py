cdc20d4c9f34fd3c81aaf4e543cd8bc10b7ab10e
skiddpd <95869000+skiddpd@users.noreply.github.com>
2021-12-09 10:39:15 -0600

    Add files via upload

---
+++ light.py
@@ -0,0 +1,58 @@
+#!/usr/local/bin/python
+
+import RPi.GPIO as GPIO
+import time
+
+__author__ = 'Gus (Adapted from Adafruit)'
+__license__ = "GPL"
+__maintainer__ = "pimylifeup.com"
+
+GPIO.setmode(GPIO.BOARD)
+
+#allow motor to move backward
+def backwards(delay, steps):
+    i = 0
+    while i in range(0, steps):
+        setStep(1, 0, 0, 1)
+        time.sleep(delay)
+        setStep(0, 1, 0, 1)
+        time.sleep(delay)
+        setStep(0, 1, 1, 0)
+        time.sleep(delay)
+        setStep(1, 0, 1, 0)
+        time.sleep(delay)
+        i += 1
+
+
+#define the pin that goes to the circuit
+pin_to_circuit = 7
+
+def rc_time (pin_to_circuit):
+    count = 0
+  
+    #Output on the pin for 
+    GPIO.setup(pin_to_circuit, GPIO.OUT)
+    GPIO.output(pin_to_circuit, GPIO.LOW)
+    time.sleep(0.1)
+
+    #Change the pin back to input
+    GPIO.setup(pin_to_circuit, GPIO.IN)
+  
+    #Count until the pin goes high
+    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+        count += 1
+
+    return count
+
+#Catch when script is interupted, cleanup correctly
+try:
+    # Main loop
+    while True:
+
+        print(rc_time(pin_to_circuit))
+        #r = requests.post('https://maker.ifttt.com/trigger/testing/with/key/bd9V2gUrd3UyETgcHA4z42zk0AFCaOevt7NUNAiMtuR',params={"value1":temp,"value2":"none","value3":"none"})
+        time.sleep(5)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
